117b521ef8022399eab801ac1e6a2f134ae0966f
Zurglots <bfeole@gmail.com>
2019-06-07 14:47:06 -0400

    added key to tabs updated readme

--- lambdatimes/src/components/Content/Tabs.js
+++ lambdatimes/src/components/Content/Tabs.js
@@ -7,21 +7,18 @@ const Tabs = props => {
     <div className="tabs">
       <div className="topics">
         <span className="title">TRENDING TOPICS:</span>
-        {props.tabs.map(tab => (
+        {props.tabs.map((tab, i) => (
           <Tab
             tab={tab}
             selectedTab={props.selectedTab}
             selectTabHandler={props.selectTabHandler}
+            key={i}
           />
         ))}
-        {/* map over the tabs provided on your props, create a new Tab component for each one.
-            give the tab component a `selectTabHandler`, the `selectedTab`, and the `tab` itself as props*/}
       </div>
     </div>
   );
 };
-
-// Make sure to use PropTypes to validate your types!
 
 Tabs.propTypes = {
   selectedTab: PropTypes.string,
