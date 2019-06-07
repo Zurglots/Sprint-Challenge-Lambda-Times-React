a3bde4aafb4c87a156cb151a93e93eeaf0a1bc16
Zurglots <bfeole@gmail.com>
2019-06-07 13:12:23 -0400

    added styled components to header and top bar components

--- lambdatimes/src/components/Header.js
+++ lambdatimes/src/components/Header.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
+import { HeaderContainer, Date, Title, Temp } from "./styles";
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const Header = () => {
   return (
-    <div className="header">
-      <span className="date">SMARCH 32, 2018</span>
-      <h1>Lambda Times</h1>
-      <span className="temp">98°</span>
-    </div>
-  )
-}
+    <HeaderContainer>
+      <Date>SMARCH 32, 2018</Date>
+      <Title>Lambda Times</Title>
+      <Temp>98°</Temp>
+    </HeaderContainer>
+  );
+};
 
-export default Header
+export default Header;
--- lambdatimes/src/components/TopBar.js
+++ lambdatimes/src/components/TopBar.js
@@ -1,24 +1,38 @@
-import React from 'react';
+import React from "react";
+import {
+  TopBarWrap,
+  TopBarContainer,
+  TopLeft,
+  TopLeftSpan,
+  TopCenterContainer,
+  TopCenterSpan,
+  TopContainerRight
+} from "./styles";
 
-// Refactor this component to use styled components and not classNames. 
+// Refactor this component to use styled components and not classNames.
 // You can find the corresponding CSS in the CSS/index.css file
 
 const TopBar = () => {
   return (
-    <div className="top-bar">
-      <div className="container">
-        <div className="container-left">
-          <span>TOPICS</span><span>SEARCH</span>
-        </div>
-        <div className="container-center">
-          <span>GENERAL</span><span>BROWNBAG</span><span>RANDOM</span><span>MUSIC</span><span>ANNOUNCEMENTS</span>
-        </div>
-        <div className="container-right">
+    <TopBarWrap>
+      <TopBarContainer>
+        <TopLeft>
+          <TopLeftSpan>TOPICS</TopLeftSpan>
+          <TopLeftSpan>SEARCH</TopLeftSpan>
+        </TopLeft>
+        <TopCenterContainer>
+          <TopCenterSpan>GENERAL</TopCenterSpan>
+          <TopCenterSpan>BROWNBAG</TopCenterSpan>
+          <TopCenterSpan>RANDOM</TopCenterSpan>
+          <TopCenterSpan>MUSIC</TopCenterSpan>
+          <TopCenterSpan>ANNOUNCEMENTS</TopCenterSpan>
+        </TopCenterContainer>
+        <TopContainerRight>
           <span>LOG IN</span>
-        </div>
-      </div>
-    </div>
-  )
-}
+        </TopContainerRight>
+      </TopBarContainer>
+    </TopBarWrap>
+  );
+};
 
 export default TopBar;
